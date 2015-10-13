fc65eaf969b960698be664fbb7d77b587d16ecd0
adek <adek13@gmail.com>
2015-10-13 21:03:59 +0200

    dodano camera controller

---
+++ Assets/Scritps/cameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+	public GameObject lookAtObject;
+	public float time=20;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (time > 0) {
+			transform.LookAt (lookAtObject.transform);
+			transform.RotateAround (lookAtObject.transform.position, Vector3.up, -20 * Time.deltaTime);
+			time-=Time.fixedDeltaTime;
+		}
+	}
+}
