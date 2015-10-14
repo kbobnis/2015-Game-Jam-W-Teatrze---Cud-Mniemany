8db81bfd91256ea2662c43e8c819542d120d8728
Philon <therealphilon@gmail.com>
2015-10-14 13:16:04 +0200

    .

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject LookAtObject;
+	public float Time=20;
+
+	void Start ()
+	{
+		if (LookAtObject == null)
+		{
+			throw new System.Exception("There is no look at object defined");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Time > 0)
+		{
+			transform.LookAt (LookAtObject.transform);
+			transform.RotateAround (LookAtObject.transform.position, Vector3.up, -20 * UnityEngine.Time.deltaTime);
+			Time-=UnityEngine.Time.fixedDeltaTime;
+		}
+	}
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,12 +4,22 @@ using System.Collections.Generic;
 
 public class Game : MonoBehaviour
 {
+	public GameObject SpectatorsRow1, SpectatorsRow2, SpectatorsRow3;
+	public GameObject FrontLayer1, FrontLayer2, FrontLayer3;
+	public GameObject WspornikiLayer, BackElementsLayer;
+	public MeshRenderer LandscapeRenderer;
+	public GameObject PairPrefab;
 
 	void Start ()
 	{
 		XMLLoader xmlLoader = new XMLLoader();
 		
 		GameModel gameModel = xmlLoader.LoadGame(Resources.Load<TextAsset>("model").text);
+	}
+
+	void InsertPair(GameObject pair)
+	{
+		throw new System.Exception("Land this pair in one of the layers");
 	}
 	
 }
