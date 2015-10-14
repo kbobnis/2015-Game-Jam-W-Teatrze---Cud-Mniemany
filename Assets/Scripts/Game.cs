e1d594f9c9378f2df2f617916a8b43d675f489be
Philon <therealphilon@gmail.com>
2015-10-14 20:01:01 +0200

    schodzą w dół

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 
 public class Game : MonoBehaviour
 {
+	public static Game Me;
+
 	public ElementLayer[] ElementLayers;
 	public PairLayer PairLayer1, PairLayer2, PairLayer3;
 	public MeshRenderer LandscapeRenderer;
@@ -12,23 +14,19 @@ public class Game : MonoBehaviour
 
 	void Start ()
 	{
+		Me = this;
 		XMLLoader xmlLoader = new XMLLoader();
-		
 		GameModel gameModel = xmlLoader.LoadGame(Resources.Load<TextAsset>("model").text);
-
 		Scene scene = gameModel.Scenes[0];
-
 		foreach (ElementLayer el in ElementLayers)
 		{
 			el.Prepare(scene.Layers[el.LayerType]);
 		}
 
 		int enemiesCount = scene.EnemiesCount;
-
 		enemiesCount -= PairLayer1.Prepare(scene.EnemiesParty1, scene.EnemiesParty2, enemiesCount, scene.Words);
 		enemiesCount -= PairLayer2.Prepare(scene.EnemiesParty1, scene.EnemiesParty2, enemiesCount, scene.Words);
 		enemiesCount -= PairLayer3.Prepare(scene.EnemiesParty1, scene.EnemiesParty2, enemiesCount, scene.Words);
-
 		GetComponent<CameraController>().Restart(scene.Time);
 		PapaMover.Restart(scene.Time);
 	}
